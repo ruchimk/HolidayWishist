ea1c38af02c64196c3c5ff6a735137d0d1d162fe
Ruchi Malik <rmalik2788@gmail.com>
2014-12-14 06:04:06 -0500

    adding relationship between items and user

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  belongs_to :user
+
   # GET /items
   # GET /items.json
   def index
