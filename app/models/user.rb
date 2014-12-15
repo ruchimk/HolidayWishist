69fcba012549623ee86cfb68169aa6170e2e7fee
Ruchi Malik <rmalik2788@gmail.com>
2014-12-14 22:38:08 -0500

    defined not_friends in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,12 @@ class User < ActiveRecord::Base
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
   has_many :inverse_friends, :through => :inverse_friendships, :source => :user
 
+  def not_friends
+    already_friends_ids = self.friends.map {|friend| friend.id }
+    not_friends = []
+    User.all.each do |user|
+      not_friends << user unless already_friends_ids.include?(user.id) || self.id == user.id
+    end
+    not_friends
+  end
 end
