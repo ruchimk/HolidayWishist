9cf2f72648f2b5ea7a28c5c62f6bfa17eb03e8cd
Ruchi Malik <rmalik2788@gmail.com>
2014-12-17 08:20:59 -0500

    deleted and resaved favicon in images

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
+  # mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}, controllers: { registrations: 'registrations' }
   devise_scope :user do
     get '/sign_up', to: 'devise/registrations#new'
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     delete 'sign_out', to: 'devise/sessions#destroy'
   end
 
-  get '/api_call', to: 'items#make_api_call'
+  # get '/api_call', to: 'items#make_api_call'
 
   resources :items
   resources :users, only: [:show]
