b3f9ac385b87c42837b055a88c404d49cefe1995
Ruchi Malik <rmalik2788@gmail.com>
2014-12-16 20:42:21 -0500

    commenting out main.js till i get to backbone

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,6 @@
+// var items = new ItemCollection();
+// var router = new ItemRouter({collection: items});
+// // Fetch collection, and then start history:
+// items.fetch().then(function() {
+//   Backbone.history.start();
+// });
