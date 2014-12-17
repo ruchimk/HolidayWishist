1e6ccf27be09d7714c7868eb07e208fddb22206d
Ruchi Malik <rmalik2788@gmail.com>
2014-12-17 08:15:21 -0500

    deleted holidaywishlist folder that was created in images

--- app/assets/images/HolidayWishlist_files/main.js
+++
@@ -1,6 +0,0 @@
-// var items = new ItemCollection();
-// var router = new ItemRouter({collection: items});
-// // Fetch collection, and then start history:
-// items.fetch().then(function() {
-//   Backbone.history.start();
-// });
