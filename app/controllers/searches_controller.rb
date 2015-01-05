19448e46fe0a48a19a4cb14aff1b76f2dfecac67
Ruchi Malik <rmalik2788@gmail.com>
2015-01-04 23:23:57 -0500

    working on css for items

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,7 +2,6 @@
 class SearchesController < ApplicationController
 
   def new
-    # Get an item: https://openapi.etsy.com/v2/listings/active?api_key=mi7mse8bmftparcsqwsyassi
     #new search should show all the listings of all categories
     url = "https://openapi.etsy.com/v2/taxonomy/categories?api_key=mi7mse8bmftparcsqwsyassi"
     response = HTTParty.get(url)
@@ -15,9 +14,7 @@ class SearchesController < ApplicationController
     category = params['name']
     url = "https://openapi.etsy.com/v2/private/listings/active?sort_on=created&sort_order=down&category=#{category}&api_key=mi7mse8bmftparcsqwsyassi&includes=MainImage&limit=100"
     response = HTTParty.get(url)
-    # images = HTTParty.get("https://openapi.etsy.com/v2/listings/#{@item.etsy_id}/images?api_key=mi7mse8bmftparcsqwsyassi")
     @found_products = response['results']
-    # @images = images['results']
   end
 
 end
